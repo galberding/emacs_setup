c8f55ec3edebedd30640fa2b5d41b696e4e1df2f
galberding <galberding@techfak.uni-bielefeld.de>
2020-07-24 15:01:26 +0200

    Add emacs setup instructions

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # Spacemacs setup
 
-## Emacs Requirerments 
+## Emacs Requirerments
 ```bash
 sudo apt-get install build-essential texinfo-doc-nonfree install-info info libx11-dev libxpm-dev libjpeg-dev libpng-dev libgif-dev libtiff-dev libgtk2.0-dev libncurses5-dev libxpm-dev automake autoconf libgnutls28-dev texinfo
 ```
 
+## Build emacs27
+
+```bash
+./build_emacs27.sh
+```
 
 ## Layer config
 ```lisp
---
+++ build_emacs27.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+set -e
+#module load gcc
+
+# Git settings
+BRANCH_NAME=emacs-27
+GIT_REPO=https://git.savannah.gnu.org/git/emacs.git
+
+# Path to external repo to place the installation
+INSTALL_REPO=
+TMP_REPO=
+# Create workspace dir
+SRC_SUFFIX=emacs
+BUILD_SUFFIX=build
+INSTALL_SUFFIX=$BRANCH_NAME
+mkdir -p $SRC_SUFFIX
+cd $SRC_SUFFIX
+
+# Define environment variables
+CURRENT_WD=`pwd`
+SRC_DIR=$CURRENT_WD/$SRC_SUFFIX
+BUILD_DIR=$CURRENT_WD/$BUILD_SUFFIX
+INSTALL_DIR=$CURRENT_WD/$INSTALL_SUFFIX
+PREFIX_DIR=$INSTALL_REPO/$INSTALL_SUFFIX
+CPU_CORES=`nproc`
+
+# Clone sources if not already existent
+if [ ! -d $SRC_DIR ]; then
+    echo "Could not find src directory, cloning from git ..."
+    git clone $GIT_REPO
+else
+    echo "Found src files."
+fi
+
+# Create intall and build dir
+cd $CURRENT_WD
+mkdir -p $BUILD_DIR $INSTALL_DIR
+
+# Checkout the desired branch
+cd $SRC_DIR
+git checkout $BRANCH_NAME
+
+# GEnerate Config
+cd $SRC_DIR
+./autogen.sh
+cd $BUILD_DIR
+
+set +e
+make clean
+set -e
+
+$SRC_DIR/configure --prefix=$INSTALL_DIR
+make -j$CPU_CORES
+make install
