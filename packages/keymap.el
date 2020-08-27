fbd476fe65202aa458feb3d3a495bd37fd74f53a
galberding <galberding@techfak.uni-bielefeld.de>
2020-08-27 14:26:28 +0200

    Add leader keys to mimic spacemacs

--- packages/keymap.el
+++ packages/keymap.el
@@ -1,8 +1,17 @@
+;; Add prefix key
+
+(define-prefix-command 'leader-map)
+(define-prefix-command 'action-map)
+
+(global-set-key (kbd "M-m") 'leader-map)
+(global-set-key (kbd "M-RET") 'action-map)
+
 ;; Keymap
 
-(global-set-key (kbd "M-RET c") 'comment-line)
-
-
+;; (global-set-key (kbd "M-RET c") 'comment-line)
+(define-key action-map (kbd "c") 'comment-line)
+(global-set-key (kbd "M-c") 'comment-line)
+(global-set-key (kbd "M-q") 'comment-line)
 ;; Multi cursor
 (global-set-key (kbd "C-<") 'mc/mark-next-like-this)
 (global-set-key (kbd "C->") 'mc/mark-previous-like-this)
@@ -11,7 +20,7 @@
 
 ;; shell
 (global-set-key (kbd "M-RET t") 'multi-term-open-split-buffer)
-
+(define-key action-map (kbd "t") ''multi-term-open-split-buffer)
 
 ;; Replace buffer list
 (global-set-key (kbd "C-x b") 'helm-buffers-list)
@@ -22,12 +31,17 @@
 ;; (define-prefix-command "space-map"  "M-m")
 
 ;; ;; git
-;; (global-set-key (kbd "M-m g s") 'magit-status)
-;; (global-set-key (kbd "M-m g c") 'magit-clone)
+(define-key leader-map (kbd "g s") 'magit-status)
+(define-key leader-map (kbd "g c") 'magit-clone)
 
 ;; Duplicate line
-(global-set-key (kbd "M-RET d l") 'duplicate-line)
-;; (global-set-key (kbd "M-m d l") 'duplicate-line)
+;; (global-set-key (kbd "M-RET d l") 'duplicate-line)
+(define-key leader-map (kbd "l d") 'duplicate-line)
 
 (global-set-key [(meta shift up)]  'move-line-up)
 (global-set-key [(meta shift down)]  'move-line-down)
+
+;; Treemacs
+(define-key leader-map (kbd "f t") 'treemacs)
+
+;; Bookmarks
